e9512534ea293d8d37266754cde0b09717e122c7
medram <mramouchy@gmail.com>
2018-12-06 16:24:09 +0000

    fill the Dashboard with data

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,6 +4,199 @@ require_once "dashboard_init.php";
 use MR4Web\Utils\View;
 use MR4Web\Utils\Dashboard;
 
+use MR4Web\Models\Transaction;
+use MR4Web\Models\Customer;
+use MR4Web\Models\Coupon;
+use MR4Web\Models\License;
+use MR4Web\Models\Product;
+
+/*
+	- show data per days/weeks/months
+	- Money: paypal fee/gross/earned 
+	- All Customers/Transactions
+	- Coupons: Used/All
+	- Licenses: Used/All
+	
+*/
+
+// time : all/week/month/day
+
+$time = isset($_GET['time']) ? $_GET['time'] : 'all'; 
+
+if ($time == 'all')
+{
+	// --------- calculate: fee/earned/gross ---------
+	$transactions = Transaction::getAll();
+	$fee = 0;
+	$earned = 0;
+	$gross = 0;
+
+	if (count($transactions))
+	{
+		foreach ($transactions as $tr)
+		{
+			$fee += $tr->Tr_fee;
+			$earned += ($tr->amount - $tr->Tr_fee);
+			$gross += $tr->amount;
+		}
+	}
+
+	$data['tr_total'] = count($transactions);
+	$data['tr_fee'] = $fee;
+	$data['tr_earned'] = $earned;
+	$data['tr_gross'] = $gross;
+
+	// --------- Total customers ---------
+	$customers = Customer::getAll();
+	$totalCustomers = 0;
+
+	if (count($customers))
+		$totalCustomers = count($customers);
+
+	$data['customers_total'] = $totalCustomers;
+
+	// --------- Licenses ---------
+	$licenses = License::getAll();
+	$used = 0;
+	$max = 0;
+	$totalLicenses = 0;
+	$banned = 0;
+
+	if (count($licenses))
+	{
+		$totalLicenses = count($licenses);
+		foreach ($licenses as $license)
+		{
+			$used += $license->activation_num;
+			$max += $license->activation_max;
+			$banned += $license->banned;
+		}
+
+		$data['licenses_used'] = $used;
+		$data['licenses_max'] = $max;
+		$data['licenses_total'] = $totalLicenses;
+		$data['licenses_banned'] = $banned;
+	}
+
+	// --------- Coupons ---------
+	$coupons = Coupon::getAll();
+	$total = 0;
+	$active = 0;
+	$inactive = 0;
+	$expired = 0;
+
+	$validTime = 0;
+	$totalValidTime = 0; 
+
+	if (count($coupons))
+	{
+		$total = count($coupons);
+		foreach ($coupons as $coupon)
+		{
+			if ($coupon->status)
+				++$active;
+			else
+				++$inactive;
+
+			if ($coupon->expired)
+				++$expired;
+			
+			$totalValidTime += $coupon->total_valid_time;
+			$validTime += $coupon->valid_time;
+		}
+
+		$data['coupon_total'] = $total;
+		$data['coupon_active'] = $active;
+		$data['coupon_inactive'] = $inactive;
+		$data['coupon_expired'] = $expired;
+		$data['coupon_valid_time'] = $validTime;
+		$data['coupon_total_valid_time'] = $totalValidTime;
+	}
+
+	// -------- Products --------
+	$products = Product::getAll(['id', 'DESC']);
+	$totalProducts = 0;
+	$data['products'] = [];
+
+	if (count($products))
+	{
+		$totalProducts = count($products);
+		$totalPlans = 0;
+
+		foreach ($products as $product)
+		{
+			// set product
+			$pr = array(
+				'name' => $product->name,
+				'version' => $product->version,
+				'email_support' => $product->email_support,
+				'created' => $product->created,
+				);
+
+			// calculate licenses
+			$licenses = $product->getLicenses();
+			$l = [];
+			if (count($licenses))
+			{
+				$used = 0;
+				$banned = 0;
+
+				foreach ($licenses as $license)
+				{
+					// just count the used licenses
+					if ($license->activation_num)
+						++$used;
+					if ($license->banned)
+						++$banned;
+				}
+
+				$l = array(
+					'total' => count($licenses),
+					'used' => $used,
+					'banned' => $banned,
+				);
+			}
+
+			// calculate profit/product 
+			$plans = $product->getPlans();
+			$transactions = [];
+			$profit = 0;
+			$gross = 0;
+
+			if (count($plans))
+			{
+				$totalPlans += count($plans);
+				foreach ($plans as $plan)
+				{
+					foreach ($plan->getInvoices() as $invoice)
+					{
+						// merge the transaction to the array.
+						$transactions[] = $invoice->getTransaction();
+					}
+				}
+			}
+
+			if (count($transactions))
+			{
+				foreach ($transactions as $tr)
+				{
+					//$fee += $tr->Tr_fee;
+					$profit += ($tr->amount - $tr->Tr_fee);
+					$gross += $tr->amount;
+				}
+			}
+
+			$data['products_rows'][] = ['product' => $pr,'licenses' => $l, 'gross' => $gross, 'profit' => $profit];
+		}
+
+		$data['products_total'] = $totalProducts;
+		$data['plans_total'] = $totalPlans;
+
+	}
+
+}
+
+
 $data['dash_title'] = 'Dashboard';
 Dashboard::Render('index', $data);
 
--- inc/views/dashboard/index.php
+++ inc/views/dashboard/index.php
@@ -1,1 +1,108 @@
-<span>Hello Dashboard</span>
+<div class="row">
+	<div class="col-md-8">
+		<h2 class="text-success"><i class="fa fa-fw fa-money"></i> Money:</h2>
+		<table class="table table-sm table-striped table-hover">
+			<thead>
+				<tr>
+					<th>Gross</th>
+					<th>Fee</th>
+					<th>Profit</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php if ($tr_gross): ?>
+				<tr>
+					<td>$<?php echo $tr_gross ?></td>
+					<td><span class="text-danger">$<?php echo $tr_fee ?></span></td>
+					<td><b class="text-success">$<?php echo $tr_earned ?></b></td>
+				</tr>
+
+				<?php else: ?>
+					<tr><td class="text-center" colspan="3">No available data for now!</td></tr>
+				<?php endif; ?>
+			</tbody>
+		</table>
+		
+		<h2 class="text-primary"><i class="fa fa-w fa-key"></i> Licenses:</h2>
+		<table class="table table-sm table-striped table-hover">
+			<thead>
+				<tr>
+					<th>Used/Max</th>
+					<th>Banned</th>
+					<th>Total Licenses</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php if ($licenses_total): ?>
+				<tr>
+					<td><?php echo $licenses_used.'/'.$licenses_max ?></td>
+					<td><span class="text-danger"><?php echo $licenses_banned ?> <i class="fa fa-key"></i></span></td>
+					<td><b class="text-primary"><?php echo $licenses_total ?> <i class="fa fa-key"></i></b></td>
+				</tr>
+
+				<?php else: ?>
+					<tr><td class="text-center" colspan="3">No available data for now!</td></tr>
+				<?php endif; ?>
+			</tbody>
+		</table>
+		
+		<h2 class="text-danger"><i class="fa fa-w fa-ticket"></i> Coupons:</h2>
+		<table class="table table-sm table-striped table-hover">
+			<thead>
+				<tr>
+					<th>Active/total</th>
+					<th>Inactive/total</th>
+					<th>Expired/Total</th>
+					<th>Valid Time / Total Valid Time</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php if ($coupon_total): ?>
+				<tr>
+					<td><?php echo $coupon_active.'/'.$coupon_total ?></td>
+					<td><?php echo $coupon_inactive.'/'.$coupon_total ?></td>
+					<td><?php echo $coupon_expired.'/'.$coupon_total ?></td>
+					<td><?php echo $coupon_valid_time.'/'.$coupon_total_valid_time ?></td>
+				</tr>
+
+				<?php else: ?>
+					<tr><td class="text-center" colspan="4">No available data for now!</td></tr>
+				<?php endif; ?>
+			</tbody>
+		</table>
+		
+		<h2 class="text-info"><i class="fa fa-w fa-cube"></i> Products (<?php echo $products_total ?>):</h2>
+		<table class="table table-sm table-striped table-hover">
+			<thead>
+				<tr>
+					<th>Product</th>
+					<th>Licenses (used/total)</th>
+					<th>Revenue (profit/gross)</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php if ($products_total): ?>
+					<?php foreach ($products_rows as $row): ?>
+					<tr>
+						<td><?php echo $row['product']['name']." <small>v".$row['product']['version']."</small>"; ?></td>
+						<td class="text-primary"><?php echo $row['licenses']['used'].'/'.$row['licenses']['total'] ?> <i class="fa fa-key"></i></td>
+						<td class="text-success"><?php echo $row['profit'].'<b>/</b>'.$row['gross'] ?> $</td>
+					</tr>
+					<?php endforeach; ?>
+		
+				<?php else: ?>
+					<tr><td class="text-center" colspan="3">No available data for now!</td></tr>
+				<?php endif; ?>
+			</tbody>
+		</table>
+	</div>
+	<div class="col-md-4">
+		<div class="text-muted">
+			<h2 class="text-info">Other</h2>
+			<b><i class="fa fa-users"></i> Customers: </b><span><?php echo $customers_total ?></span><br>
+			<b><i class="fa fa-credit-card"></i> Transactions: </b><span><?php echo $tr_total ?></span><br>
+			<b><i class="fa fa-cube"></i> Products: </b><span><?php echo $products_total ?></span><br>
+			<b><i class="fa fa-flag-o"></i> Plans: </b><span><?php echo $plans_total ?></span><br>
+		</div>
+	</div>
+</div>
